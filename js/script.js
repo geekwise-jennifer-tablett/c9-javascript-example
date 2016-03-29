a39f23e99f8e2fe8e1c8eba40a1579e343aeffd3
geekwise_jennifer_tablett <geekwise.jennifer.tablett@gmail.com>
2016-03-29 22:04:40 +0000

    added

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var h1
+var createTag = function{
+    h1 = document.addEventListener('h1')
+    h1.textContent = 'my h1 tag'
+    document.body.appendChild(h1);
+var colors = []
+
+    'red',
+    'blue'
+    'gold',
+    'grey',
+    'coral'
+];
+var createTag = function(){
+    h1 = document.createElement('h1');
+    h1.textContent = 'my h1 tag';
+    document.body.appendChild(h1);
+
+var randomPixel = function(){
+    var number = Math.round(
+        Math.random()*500)+1;
+        return number + 'px';
+}
+
+var randomColor = function(){
+    var randomNumber = Math.round(
+        Math.random() * color.length)- 1;
+}
+
+document.addEventListener(DOMContentLoaded',function)
