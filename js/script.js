7cb38cdaef6fbb87606ab3d7547ecea5fb53dca4
geekwise_jennifer_tablett <geekwise.jennifer.tablett@gmail.com>
2016-03-30 23:44:37 +0000

    click trial

--- js/script.js
+++ js/script.js
@@ -1,30 +1,117 @@
-var h1
-var createTag = function{
-    h1 = document.addEventListener('h1')
-    h1.textContent = 'my h1 tag'
-    document.body.appendChild(h1);
-var colors = []
+var div;
 
-    'red',
-    'blue'
-    'gold',
-    'grey',
-    'coral'
-];
-var createTag = function(){
-    h1 = document.createElement('h1');
-    h1.textContent = 'my h1 tag';
-    document.body.appendChild(h1);
+document.addEventListener('DOMContentLoaded',function(event){
+   div =document.createElement('div');
+   
+   document.body.appendChild(div)
+ 
+ 
+    var description = 'Enter A CSS Color Or Hex Color - exmaple:#fff444 or Red'
+var create_html_element = function (name_of_element,name_of_id) {
+    var element = document.createElement(name_of_element);
+  element.setAttribute('id',name_of_id);
+  
+  element.textContent = name_of_element + ' tag';
+  document.body.appendChild(element);
+   console.log(true);
 
-var randomPixel = function(){
-    var number = Math.round(
-        Math.random()*500)+1;
-        return number + 'px';
-}
+};
 
-var randomColor = function(){
-    var randomNumber = Math.round(
-        Math.random() * color.length)- 1;
-}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+   /*click_begins*/
+    div.addEventListenter('click',function(event){
+    
+    
+    id.innerHtml = 'Click Me Please'
+     div.addEventListener('change text', function( event ) {
+    
+     event.target.style.background = "green";    
+        }, true);  function changeText(id) {
+    
+    id.innerHTML = 'Oh No!';
+    }
+});
+  
+  
+  
+  
+  
+  
+}); 
+   /*click_ends*/
+   
+   
+  
+  
+  
+  
+  
+  
+   /*key_down_begins*/
 
-document.addEventListener(DOMContentLoaded',function)
+
+
+document.addEventListener('DOMContentLoaded', function(event){
+   create_html_element('div', 'div_box_text');
+   create_html_element('span','span_text');
+   span_text.textContent = description;
+   div_box_text.setAttribute('placeholder','enter a css color name');
+   
+   div_box_text.addEventListener('keydown',function(event){
+       
+       console.log(event.keyCode);
+       if(event.keyCode === 13){
+           span_text.textContent = div_box_text.value;
+           span_text.style.backgroundColor = 
+           div_box.value;
+         document.body.style.backgroundColor = div_box_test.value; 
+       };
+    
+    
+ });
+
+   /*key_down_ends*/
+  
+  
+ 
+   /*mouse_over_begins*/
+  /* div.addEventListenter('mouse_over',function(event){
+     
+      onmouseover='mOver(this)' onmouseout='mOut(this)' 
+
+     style="background-color:orange; 
+   });*/
+   /*mouse_over_ends*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+    
+
+
+
+
