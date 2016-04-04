2b67fbe93c0e2547c1f3a251addcb2bb8648dfc4
geekwise-jennifer-tablett <geekwise.jennifer.tablett@gmail.com>
2016-04-04 22:22:00 +0000

    added input box event

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 var createElement(ul);
-var createElement[li '10-50'];
+var createElement(li);
 
+/*input_box_for_adding_li_to_body*/
+var create_html('input_box');
 
-var createElement(input_tag,'createElement');
+ 
+
 /*giut
 for(var i=10, i<50, i++){
  var list_element = document.createElement('li');
@@ -11,22 +14,31 @@ for(var i=10, i<50, i++){
  ul_id.appendChild(list_element);
 }
 */
-
+/*for_changing_background-color_of_li*/
 var createElement(input_tag,'change_color');
   setAttribute('type','text');
   setAtribute('value','input_tag')
 
 
 
-
+/*for_deleting_element_from_body*/
 var createElement(input_tag,'DeleteElement');
 
 document.addEventListener('DOMContentListener',function(event){
  console.log(this);
+ create_html_element('input', 'input_box');
+    create_html_element('span','span_text');
+    span_text.textContent = description;
+    input_box.setAttribute('placeholder','number_li');
+    
+    input_box.addEventListener('keydown',function(event){
+        
+        console.log(event.keyCode);
+        if(event.keyCode === 13){
+            span_text.textContent = input_box.value;
+    }       
  
-})
- 
- 
+ });
 
  
  
