9658f46699c11a077e6d9d9345b418a6cc36a3ef
geekwise_jennifer_tablett <geekwise.jennifer.tablett@gmail.com>
2016-04-04 20:47:39 +0000

    shared box

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 var createElement(ul);
 var createElement[li '10-50'];
 
+
 var createElement(input_tag,'createElement');
-/*
+/*giut
 for(var i=10, i<50, i++){
  var list_element = document.createElement('li');
  list_element.setAttribute(Attribute('id', 'list_id_',+i);
