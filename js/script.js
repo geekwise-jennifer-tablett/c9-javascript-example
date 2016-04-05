d9b78f68bd070b573b87508b4fe61ad7b6f3caa3
geekwise-jennifer-tablett <geekwise.jennifer.tablett@gmail.com>
2016-04-05 22:02:40 +0000

    if and else practice

--- js/script.js
+++ js/script.js
@@ -1,53 +1,40 @@
-var createElement(ul);
-var createElement(li);
+var big_button;
+var background_button
 
-/*input_box_for_adding_li_to_body*/
-var create_html('input_box');
 
- 
-
-/*giut
-for(var i=10, i<50, i++){
- var list_element = document.createElement('li');
- list_element.setAttribute(Attribute('id', 'list_id_',+i);
- list_element.textContent = 'item#'+i;
- ul_id.appendChild(list_element);
-}
-*/
-/*for_changing_background-color_of_li*/
-var createElement(input_tag,'change_color');
-  setAttribute('type','text');
-  setAtribute('value','input_tag')
+var change_button_color = function(change_button_color){
+   if(big_button.style.backgroundColor === 'blue') {
+       
+       big_button.style.backgroundColor = 'red';
+   }else{
+       
+       big_button.style.backgroundColor = 'green';
+   };
+  
+  };
 
 
 
-/*for_deleting_element_from_body*/
-var createElement(input_tag,'DeleteElement');
 
-document.addEventListener('DOMContentListener',function(event){
- console.log(this);
- create_html_element('input', 'input_box');
-    create_html_element('span','span_text');
-    span_text.textContent = description;
-    input_box.setAttribute('placeholder','number_li');
-    
-    input_box.addEventListener('keydown',function(event){
-        
-        console.log(event.keyCode);
-        if(event.keyCode === 13){
-            span_text.textContent = input_box.value;
-    }       
- 
- });
 
- 
- 
- 
- 
- 
- 
- 
+
    
+document.addEventListener('DOMContentLoaded',function(event){
+    
+    big_button = document.getElementById('big_button');
+    big_button.style.backgroundColor = 'blue';
+    
+    big_button.addEventListener('click',function(event){
+    
+    change_button_color();
+        
+        console.log(this);
+        
+    });
+    
+    
+});
+
 
 
 
