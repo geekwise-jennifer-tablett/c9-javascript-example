c5638ad78163315b143a596a8df9c992a23c6b8c
geekwise-jennifer-tablett <geekwise.jennifer.tablett@gmail.com>
2016-04-06 20:25:03 +0000

    pushing to gh

--- js/script.js
+++ js/script.js
@@ -1,66 +1,19 @@
-var big_button;
+var photo_container;
+var img_urls = new Array;
 
-/*lets see if this works*/
-var speed = prompt ('speed limit is 55', 55);
-    if(speed >= 55){
-        alert('congrats, you just donated the city your hard earned cash');
-    }else{
-        alert('go faster, the city wants your cash');
-    }
-    
-    
+var add_images = function(number_of_images){
+    var image;
+    for(var i=0; i< number_of_images; i++){
+        img_urls.push('/images/profile_photo_' + (i + 1) + '.jpg');
+        image = document.createElement('img');
+        image.setAttribute('src', img_urls[i]);
+        document.body.appendChild(image);
+    };
+};
 
-
-  
-
-
-
-
-
-
-
-
-var change_button_color = function(change_button_color){
-   if(big_button.style.backgroundColor === 'blue') {
-       
-       big_button.style.backgroundColor = 'red';
-   }else{
-       
-       big_button.style.backgroundColor = 'green';
-   };
-  
-  };
-
-
-
-
-
-
-   
-document.addEventListener('DOMContentLoaded',function(event){
-    
-    big_button = document.getElementById('big_button');
-    big_button.style.backgroundColor = 'blue';
-    
-    big_button.addEventListener('click',function(event){
-    
-    change_button_color();
-        
-        console.log(this);
-        
-    });
-    
-   
-    
-    
-/*just wondering if this works could be fun to mess with*/
-
-  
-   
-
+document.addEventListener('DOMContentLoaded', function(event){
+    add_images(68);
 });
-
-
 
 
 
