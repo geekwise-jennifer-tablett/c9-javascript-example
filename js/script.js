8860f878effeb082f68269263b8d92e51627586a
geekwise_jennifer_tablett <geekwise.jennifer.tablett@gmail.com>
2016-04-07 21:41:22 +0000

    added flip

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var photo_container;
+
 var img_urls = new Array;
 
 var add_images = function(number_of_images){
@@ -7,12 +7,52 @@ var add_images = function(number_of_images){
         img_urls.push('/images/profile_photo_' + (i + 1) + '.jpg');
         image = document.createElement('img');
         image.setAttribute('src', img_urls[i]);
+        
+        /*flip try*/
+    image.style.transform = 'rotateY(0deg)';
+    
+    image.addEventListener('click',function(){
+        
+        if(image.style.transform === 'rotateY(0deg)'){
+            image.style.transform = 'rotateY(180deg)'
+        }else{
+            image.style.transform = 'rotateY(180deg)'
+        }
+        
+        
+    });
+   
+        
         document.body.appendChild(image);
     };
 };
-// test
+
+
+
+
+
+
+/*
+('.flip') ('onclick',function() {
+  (this).find('.card').addClass('flipped')
+    .mouseleave(function() {
+    (this).removeClass('flipped');
+   
+    console.log(this);
+    
+  });
+  return false;
+
+});
+*/
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', function(event){
     add_images(68);
+    
     console.log('updated github');
 });
 
