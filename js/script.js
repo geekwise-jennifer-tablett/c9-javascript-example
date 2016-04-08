6a6162c47e2f8f056eac3aa4b7c98c1e7543dce2
geekwise_jennifer_tablett <geekwise.jennifer.tablett@gmail.com>
2016-04-08 19:03:22 +0000

    tried webkit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,312 @@
+var card_flip = function(){
+      this.classList.toggle('on');
+      this.classList.toggle('off');
+}
+
+var create_images = function(){
+
+  
+
+        for(var i=1;i<=68;i++){
+               
+               
+                var image_container = document.createElement('section');
+                
+                image_container.setAttribute('id','img_container_'+i);
+                document.body.appendChild(image_container);
+                
+                var image = document.createElement('img');
+                image.setAttribute('src','images/profile_photo_'+i+'.jpg');
+                
+                image.setAttribute('id', 'img_'+i);
+
+                
+                image.classList.add('on');
+                
+                
+                image.addEventListener('click',function(event){
+                   
+                      
+                      if(this.getAttribute('class') === 'on'){
+                              this.setAttribute('class','off');
+                              this.parentElement.children[1].setAttribute('class','on');
+                              
+                              
+                      }else{
+                              this.setAttribute('class','on');
+                              this.parentElement.children[1].setAttribute('class','off');
+                              
+                      }
+                      
+                                            event.preventDefault();
+
+                      
+                      console.log(this);
+                        
+                });
+                
+                
+                
+                
+                
+                
+                image_container.appendChild(image);
+                
+                
+                
+                var image_info = document.createElement('span');
+                
+                image_info.setAttribute('id','img_info_container_'+i);
+                image_info.classList.add('off');
+                
+                
+                image_info.addEventListener('click',function(event){
+                        
+                     if(this.getAttribute('class') === 'on'){
+                              this.setAttribute('class','off');
+                              this.parentElement.children[0].setAttribute('class','on');
+                              
+                              
+                      }else{
+                              this.setAttribute('class','on');
+                              this.parentElement.children[0].setAttribute('class','off');
+                              
+                      }
+                           event.preventDefault();
+
+                      
+                      console.log(this);
+                        
+                        
+                        
+                });
+                
+                
+                
+                
+                
+                var image_info_github_link = document.createElement('a');
+                var image_info_twitter_link = document.createElement('a');
+                
+                image_info_github_link.setAttribute('id','img_info_github_link_'+i);
+                image_info_github_link.setAttribute('href','http://www.github.com/geekwise');
+                image_info_github_link.textContent = 'github link '+i;
+                
+                image_info_twitter_link.setAttribute('id','img_info_twitter_link_'+i);
+                image_info_twitter_link.setAttribute('href','http://www.twitter.com/climateamante');
+                image_info_twitter_link.textContent = 'twitter link'+i;
+                
+                image_container.appendChild(image_info);
+                image_info.appendChild(image_info_github_link);
+                image_info.appendChild(image_info_twitter_link);
+                
+                
+                
+                
+                
+                
+                
+        };
+        
+
+}
+
+
+document.addEventListener('DOMContentLoaded',function(event){
+
+        create_images();
+        
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//webkit try
+
+/*var createElement = photo_container;
+var createElement = ('id','img_flip');
+var createElement = ('id','info_flip');
 
 var img_urls = new Array;
 
@@ -8,16 +317,56 @@ var add_images = function(number_of_images){
         image = document.createElement('img');
         image.setAttribute('src', img_urls[i]);
         
+    };
+    */
+    
+    //section for images
+   // appendChild(photo_container);
+   // photo_container.setAttribute('id','image_container_' +i);
+    
+    //images 
+   // var image= document.createElement('img');
+   // image.setAttribute('src', 'image/profile_photo_' +i+ '.jpg');
+   // image.setAttribute('id', 'image_' +i);
+   // image.classList.add('image_flip');
+    
+    
+    //info
+   // var image_info_github_link = document.CreateElement('a');
+   // var image_info_twitter_link = document.CreateElement('a');
+    
+   // image_info_github_link.setAttribute('id','img_info_link_' +i);
+   // image_info_github_link.setAttribute('href','http:www.github.com/');
+   // image_info_github_link.textContent = 'github link' +i;
+    
+   // image_info_twitter_link.setAttribute('id','img_info_link_' +i);
+   // image_info_twitter_link.setAttribute('href','http:www.twitter.com/');
+   // image_info_twitter_link.textContent = 'twitter link' +i;
+    
+   // image_info.appendChild(image_info_github_link);
+   // image_info.appendChild(image_info_twitter_link);
        
-       
-        /*flip try*/
-    image.style.transform = 'rotatey(180deg)';
+
+    /*
+    photo_container.addEventListener('click',function(event){
+        if(this.getAttribute('class')==='image_flip'){
+            this.getAttribute('class') = 'info_flip'
+        }else{
+            this.getAttribute('class') = 'image_flip'
+        }
+    });
+    
+    */
     
     
     
     
     
+     /*       document.body.appendChild(image);
+    };
+};
     
+/
     // image.addEventListener('click',function(event){
         
     //     if(image.style.transform === 'rotateY(180deg)'){
@@ -30,9 +379,7 @@ var add_images = function(number_of_images){
     // });
    
         
-        document.body.appendChild(image);
-    };
-};
+
 
 
 
@@ -55,7 +402,7 @@ var add_images = function(number_of_images){
 */
 
 
-
+/*
 
 
 document.addEventListener('DOMContentLoaded', function(event){
@@ -64,7 +411,7 @@ document.addEventListener('DOMContentLoaded', function(event){
     console.log();
 });
 
-
+*/
 
 
 
