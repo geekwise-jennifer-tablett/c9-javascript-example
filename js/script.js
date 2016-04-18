04bc6f2ed36d726cde2b9b32ddb955d49c1481bf
geekwise_jennifer_tablett <geekwise.jennifer.tablett@gmail.com>
2016-04-18 20:10:51 +0000

    added display

--- js/script.js
+++ js/script.js
@@ -13,6 +13,12 @@ var calc_button = [
     '='
     ];
 
+//creating display    
+var input;
+ input = document.createElement('input'); 
+ input.setAttribute('id','input');
+    
+
 //design of calculator body
 calc_body = document.createElement('div');
 calc_body.style.backgroundColor = 'yellow';
@@ -20,10 +26,10 @@ calc_body.style.width = '50rem';
 calc_body.style.height = '60rem';
 
 //design of button
-current_element.textContent = calc_button[i];
-        current_element.style.backgroundColor = 'black';
-        current_element.style.width = '2rem';
-        current_element.style.height = '2rem';
+// current_element.textContent = calc_button[i];
+//         current_element.style.backgroundColor = 'black';
+//         current_element.style.width = '2rem';
+//         current_element.style.height = '2rem';
  var create_element = function(element_type, element_id){
      var element = document.createElement(element_type);
      element.setAttribute('id',element_id);
@@ -32,34 +38,33 @@ current_element.textContent = calc_button[i];
 
 
 
- var input = document.getElementById('input');  
+ 
+ 
     
 document.addEventListener('DOMContentLoaded',function(event){
+  
   //appending objects
   document.body.appendChild(calc_body);
   calc_body.appendChild(input);
   
   
-  //creating display    
- create_element('input','input');
-    input.setAttribute('type', 'text');  
+   
     
  
-   //loop for button 
+  //loop for button 
     for(var i=0; i<calc_button.length; i++){
         create_element('button','calc_button_'+i);
-        
         var current_element = document.getElementById('calc_button_'+i);
-    }    
+    };
         
-        current_element.addEventListener('click',function(event){
-            input.value =  input.value += this.textContent;
-        } 
-);
+       
+        
+});
     
     
     
-    
+// current_element.addEventListener('click',function(event){
+//             input.value =  input.value += this.textContent;    
     
     
       
