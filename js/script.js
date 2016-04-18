61411c02e36bea76460b0391e4a0aed2556717ee
geekwise_jennifer_tablett <geekwise.jennifer.tablett@gmail.com>
2016-04-18 23:37:22 +0000

    added transition

--- js/script.js
+++ js/script.js
@@ -5,17 +5,22 @@ var input_element;
 var clear_container;
 var clear_button;
 var number_container;
-var number_buttons
+var number_buttons;
 var operator_container;
 var operator_buttons;
 
 //array for intergers
 number_buttons = [
-    '1',
-    '2',
-    '3',
+    '9',
+    '8',
+    '7',
+    '6',
+    '5',
     '4',
-    '5'
+    '3',
+    '2',
+    '1',
+    '0'
  ];
 
 //array for operations
@@ -30,8 +35,8 @@ operator_buttons = [
 //calculator body design
 calc_body = document.createElement('div');
 calc_body.style.backgroundColor = '#130f7e';
-calc_body.style.width = '50rem';
-calc_body.style.height = '80rem';
+calc_body.style.width = '20rem';
+calc_body.style.height = '40rem';
 
 //setting id for containers
 input_element = document.createElement('input');
@@ -62,15 +67,63 @@ var create_operators_element = function(operator_element_type,operator_element_i
     
 };
 
+
+var check_keyboard_value = function(value_to_check){
+    
+      return value_to_check.match(/\d/gi);  
+};
+
+
+var listen_for_keycode = function(){
+    
+    document.body.addEventListener('keydown',function(event){
+        //console.log(event.code);
+        //console.log(event.keyCode);
+        
+        key_pressed = event.code.match(/\d/gi);
+        
+        if(event.code.match(/digit/gi)){
+           //number_button_    
+           console.log(key_pressed);
+           console.log(document.getElementById(('number_button_'+
+                        parseInt(key_pressed[0]-1)
+            )));
+            
+            
+            var current_key_number_pressed = document.getElementById(('number_button_'+parseInt(key_pressed[0]-1)));
+            
+            
+            //current_key_number_pressed.setAttribute('class','pressed')
+            
+            
+           display.value += event.code.match(/\d/gi); 
+        }
+        
+        
+    })
+    
+    
+    
+    
+    
+};
+
+
+
+
+
+
 //when webpage loads
 document.addEventListener('DOMContentLoaded',function(event){
+    
+    listen_for_keycode();
     
     document.body.appendChild(calc_body);
     calc_body.appendChild(input_element);
     calc_body.appendChild(number_container);
     calc_body.appendChild(operator_container);
     
-    input_element_container = document.getElementById('display');
+   var display = document.getElementById('display');
     
     //loop for buttons
     for(var i=0; i<number_buttons.length; i++){
@@ -82,13 +135,13 @@ document.addEventListener('DOMContentLoaded',function(event){
         //click for display
         current_number_element.addEventListener('click',function(event){
             
-            input_element.value =  input_element.value += this.textContent;
+            display.value =  display.value += this.textContent;
             
         });
         
         current_number_element.textContent = number_buttons[i];
         
-        number_container.appendChild(current_number_element);
+        // number_container.appendChild(current_number_element);
         
     };
     
@@ -101,41 +154,48 @@ document.addEventListener('DOMContentLoaded',function(event){
         //click for operator function
         current_operator_element.addEventListener('click',function(event){
             
-            input_element.value = input_element.value += this.textContent;
+            display.value = display.value += this.textContent;
             
         });
         
         current_operator_element.textContent = operator_buttons[i];
         
-        operator_container.appendChild(current_operator_element);
+        // operator_container.appendChild(current_operator_element);
     };
     
-     calc_body.appendChild(number_container);
-     calc_body.appendChild(operator_container);
+    //  calc_body.appendChild(number_container);
+    //  calc_body.appendChild(operator_container);
    
    //create equal button and clear button
     create_numbers_element('button','equal_button');
     create_numbers_element('button','clear_button');
     
     var equal_button = document.getElementById('equal_button');
-    var clear_data_button = document.getElementById('clear_button');
-    
+    var clear_button = document.getElementById('clear_button');
+
     equal_button.textContent = '=';
     clear_button.textContent = 'C';
     
+    
    
     
-    equal_button.addEventListener('click',function calculate(equation) {
-     var answer = eval(equation);
-     document.getElementById('display').value = answer;
+    equal_button.addEventListener('click',function (equation) {
+     
+     if(display.value.length === 0){
+         display.value = 0;
+     }
+     
+     display.value = eval(display.value);
+     console.log(this);
         
     });
     
     //making clear button work
     clear_button.addEventListener('click',function(event){
-        
-        input_element.value = '';
-        
+       // document.getElementById()
+        //    display.value = '';
+        console.log(this);
+        display.value = null;
     });
     
 });
